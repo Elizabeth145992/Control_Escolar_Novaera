1ffd3d9fb3d0a2de40fffb1ef48f02d655a514f5
Elizabeth145992 <37152706+Elizabeth145992@users.noreply.github.com>
2021-08-24 21:04:38 -0500

    Termino del front-end para la primera vista principal enfocada en el alumno

--- view/header.php
+++ view/header.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://kit.fontawesome.com/220ce73536.js" crossorigin="anonymous"></script>
-    <!--Link css de Bootstrap-->
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/style.css">
-    <title>Instituto Novaera</title>
-
+    <title>Instituo Novaera</title>
+  </head>
     
