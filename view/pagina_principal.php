69c24fafdf27ed8ed843825c1aa41a8b2d33f8ca
Elizabeth145992 <37152706+Elizabeth145992@users.noreply.github.com>
2021-08-25 21:50:14 -0500

    Realización de la primra vista para que sea responsiva para tableta y celular

--- view/pagina_principal.php
+++ view/pagina_principal.php
@@ -5,56 +5,65 @@ include("header.php");
     include("principal.html");
 
     ?>
-    <div class="contenedor">
-        <p class="clases">Clases: 3° Grado de Secundaria<p>
+    
+    
+        <p class="clases">Clases: 3° Grado de Secundaria</p>
             <hr class="linea">
             <div class="row row-cols-1 row-cols-md-2 g-4">
   <div class="col">
-    <div class="card borde sombra">
-      <img class="img_clase" src="../img/matematicas.jpg" class="card-img-top" alt="...">
+    <div class="card borde sombra tamano">
+      <img class="img-fluid" src="../img/artes.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">Matemáticas 3° Grado</h5>
         <h6>Ing. Ana Ruiz</h6>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>  
+        <a href="#" class="btn btn-primary">Ver</a>  
     </div>
     </div>
   </div>
   <div class="col">
     <div class="card borde sombra">
-      <img class="img_clase" src="../img/ciencias.jpg" class="card-img-top" alt="...">
+      <img class="img-fluid" src="../img/ingles.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">Química 3° Grado</h5>
         <h6>Ing. Ana Ruiz</h6>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>  
+        <a href="#" class="btn btn-primary">Ver</a>  
     </div>
     </div>
   </div>
   <div class="col">
     <div class="card borde sombra">
-      <img class="img_clase" src="../img/ingles.jpg" class="card-img-top" alt="...">
+      <img class="img-fluid" src="../img/ingles.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">Inglés 3° Grado</h5>
         <h6>Ing. Ana Ruiz</h6>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>  
+        <a href="#" class="btn btn-primary">Ver</a>  
     </div>
     </div>
   </div>
   <div class="col">
     <div class="card borde sombra">
-      <img class="img_clase" src="../img/artes.jpg" class="card-img-top" alt="...">
+      <img class="img-fluid" src="../img/artes.jpg" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">Artes 3° Grado</h5>
         <h6>Ing. Ana Ruiz</h6>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>  
+        <a href="#" class="btn btn-primary">Ver</a>  
+    </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card borde sombra">
+      <img class="img-fluid" src="../img/artes.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Artes 3° Grado</h5>
+        <h6>Ing. Ana Ruiz</h6>
+        <a href="#" class="btn btn-primary">ver</a>  
     </div>
     </div>
   </div>
 </div>
+
 <br><br>
+</div><!--Termina contenedor-->
 
 
               <?php 
